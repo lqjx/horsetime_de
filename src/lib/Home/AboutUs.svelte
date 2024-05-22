b083a1b1de27e1eda997cc6b0be1a87dbd5e149f
retro <motionbusinessde@gmail.com>
2024-05-22 15:35:55 +0200

    Added About Us component. Needs to be worked on

---
+++ src/lib/Home/AboutUs.svelte
@@ -0,0 +1,12 @@
+<section class="flex justify-center py-16 text-center">
+    <div class="prose max-w-[1000px]">
+        <h1>Das sind wir</h1>
+        <div class="grid grid-cols-2 gap-4">
+            <div>
+                <img src="https://fakeimg.pl/300x300" alt="Raven Muenster" class="rounded-lg drop-shadow-lg"/>
+                <h2>Anja Raven Münster</h2>
+                <p class="">Hier kommt text</p>
+            </div>
+        </div>
+    </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,12 @@
     import Hero from "$lib/Home/Hero.svelte";
     import Services from "$lib/Home/Services.svelte";
     import GlobalFooter from "$lib/GlobalFooter.svelte";
+    import AboutUs from "$lib/Home/AboutUs.svelte";
 </script>
 
 <div transition:fade>
     <Hero/>
     <Services/>
+    <AboutUs/>
     <GlobalFooter/>
 </div>

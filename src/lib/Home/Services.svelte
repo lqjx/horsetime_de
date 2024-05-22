3b5d3c76b075fba5135348f08dce063b4216b0b6
retro <motionbusinessde@gmail.com>
2024-05-22 11:46:25 +0200

    Added Read More Button to home page

--- src/lib/Home/Services.svelte
+++ src/lib/Home/Services.svelte
@@ -2,8 +2,8 @@
 
 </script>
 
-<section class="flex justify-center py-16 text-center">
-    <div class="prose">
+<section class="flex justify-center py-16 text-center bg-red-50/50">
+    <div class="prose max-w-[1000px]">
         <h1>Unser Angebot</h1>
         <p>
             Auf unserer Website bieten wir vielfältige Reitangebote
@@ -13,12 +13,48 @@
             in Pferdepflege, Reittechnik und Sicherheit an. Entdecken Sie die Faszination
             des Reitens in einer freundlichen und professionellen Umgebung!
         </p>
-        <div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class="injected-svg" xmlns:xlink="http://www.w3.org/1999/xlink" role="img" color="#000000">
-                <path d="M16 22V21.5C16 20.3954 16.9321 19.5005 17.9223 19.011C18.8846 18.5354 19.6943 17.7511 19.7965 16.8313L20 15L22 14L19.5 10.25C19.5 5.69365 15.8063 2 11.25 2C10.096 2 8.99733 2.23694 8 2.66482M6.5 16.9962V22M6.5 16.9962C5.13927 16.0364 4.08188 14.6752 3.5 13.085M6.5 16.9962C7.25065 17.5257 8.09362 17.9331 9 18.1895" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                <path opacity="0.4" d="M2.75 3.25L6 5.5H10M3.5 3.25C3.5 3.66421 3.16421 4 2.75 4C2.33579 4 2 3.66421 2 3.25C2 2.83579 2.33579 2.5 2.75 2.5C3.16421 2.5 3.5 2.83579 3.5 3.25Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                <path opacity="0.4" d="M2.75 8.75H10M3.5 8.75C3.5 9.16421 3.16421 9.5 2.75 9.5C2.33579 9.5 2 9.16421 2 8.75C2 8.33579 2.33579 8 2.75 8C3.16421 8 3.5 8.33579 3.5 8.75Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>
+        <div class="md:flex justify-center space-y-4 md:space-y-0 md:space-x-4 max-w-[1000px]">
+            <div class="bg-white p-4 rounded-lg drop-shadow-md mx-4 md:mx-0">
+                <div class="flex justify-center -mb-3 gap-x-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" class="injected-svg" role="img" color="#ec4899">
+                        <path d="M19.4626 3.99415C16.7809 2.34923 14.4404 3.01211 13.0344 4.06801C12.4578 4.50096 12.1696 4.71743 12 4.71743C11.8304 4.71743 11.5422 4.50096 10.9656 4.06801C9.55962 3.01211 7.21909 2.34923 4.53744 3.99415C1.01807 6.15294 0.221721 13.2749 8.33953 19.2834C9.88572 20.4278 10.6588 21 12 21C13.3412 21 14.1143 20.4278 15.6605 19.2834C23.7783 13.2749 22.9819 6.15294 19.4626 3.99415Z" stroke="#ec4899" stroke-width="2" stroke-linecap="round"></path>
+                    </svg>
+                    <h3 class="mt-0">Reiten</h3>
+                </div>
+                <div>
+                    <p class="min-w-96 md:min-w-0 md:max-w-96">Unsere Reitstunden ermöglichen Anfängern und Fortgeschrittenen jeden Alters, ihre Fähigkeiten zu verbessern und die Freude am Reiten zu entdecken.</p>
+                </div>
+            </div>
+
+            <div class="bg-white p-4 rounded-lg drop-shadow-md max-w-[1000px] mx-4 md:mx-0">
+                <div class="flex justify-center -mb-3 gap-x-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" class="injected-svg" role="img" color="#ec4899">
+                        <path d="M13.5 4.5C13.5 5.32843 12.8284 6 12 6C11.1716 6 10.5 5.32843 10.5 4.5C10.5 3.67157 12 2 12 2C12 2 13.5 3.67157 13.5 4.5Z" stroke="#ec4899" stroke-width="2" stroke-linejoin="round"></path>
+                        <path d="M12 6V9" stroke="#ec4899" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M17.6667 14C19.2315 14 20.5 12.8807 20.5 11.5C20.5 10.1193 19.2315 9 17.6667 9H6.33333C4.76853 9 3.5 10.1193 3.5 11.5C3.5 12.8807 4.76853 14 6.33333 14C7.70408 14 8.90415 13.1411 9.16667 12C9.42919 13.1411 10.6293 14 12 14C13.3707 14 14.5708 13.1411 14.8333 12C15.0959 13.1411 16.2959 14 17.6667 14Z" stroke="#ec4899" stroke-width="2" stroke-linejoin="round"></path>
+                        <path d="M5 14L5.52089 16.5796C6.04532 19.1768 6.30754 20.4754 7.19608 21.2377C8.08462 22 9.33608 22 11.839 22H12.161C14.6639 22 15.9154 22 16.8039 21.2377C17.6925 20.4754 17.9547 19.1768 18.4791 16.5796L19 14" stroke="#ec4899" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                    <h3 class="mt-0">Geburtstage</h3>
+                </div>
+                <div>
+                    <p class="min-w-96 md:min-w-0 md:max-w-96">Wir gestalten unvergessliche Kindergeburtstage, bei denen die Kinder spielerisch den Umgang mit Pferden lernen und aufregende Reitabenteuer erleben.</p>
+                </div>
+            </div>
+
+            <div class="bg-white p-4 rounded-lg drop-shadow-md max-w-[1000px] mx-4 md:mx-0">
+                <div class="flex justify-center -mb-3 gap-x-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" class="injected-svg" role="img" color="#ec4899">
+                        <path d="M2 8C2 9.34178 10.0949 13 11.9861 13C13.8772 13 21.9722 9.34178 21.9722 8C21.9722 6.65822 13.8772 3 11.9861 3C10.0949 3 2 6.65822 2 8Z" stroke="#ec4899" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M5.99414 11L6.23925 16.6299C6.24415 16.7426 6.25634 16.8555 6.28901 16.9635C6.38998 17.2973 6.57608 17.6006 6.86 17.8044C9.08146 19.3985 14.8901 19.3985 17.1115 17.8044C17.3956 17.6006 17.5816 17.2973 17.6826 16.9635C17.7152 16.8555 17.7274 16.7426 17.7324 16.6299L17.9774 11" stroke="#ec4899" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M20.4734 9.5V16.5M20.4734 16.5C19.6814 17.9463 19.3312 18.7212 18.9755 20C18.8983 20.455 18.9596 20.6843 19.2732 20.8879C19.4006 20.9706 19.5537 21 19.7055 21H21.2259C21.3876 21 21.5507 20.9663 21.6838 20.8745C21.9753 20.6735 22.0503 20.453 21.9713 20C21.6595 18.8126 21.2623 18.0008 20.4734 16.5Z" stroke="#ec4899" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                    <h3 class="mt-0">Ausbildung</h3>
+                </div>
+                <div>
+                    <p class="min-w-96 md:min-w-0 md:max-w-96">Wir bieten Ausbildungen in Pferdepflege, Reittechnik und Sicherheit, um Ihr Wissen und Ihre Fähigkeiten im Umgang mit Pferden zu erweitern.</p>
+                </div>
+            </div>
         </div>
+        <a href="/login" class="flex w-max m-auto transition no-underline bg-pink-500 text-white p-2 rounded-lg hover:bg-pink-600 active:scale-95 mt-8">Mehr Info</a>
     </div>
 </section>
--- src/lib/Navigation/GlobalNavigation.svelte
+++ src/lib/Navigation/GlobalNavigation.svelte
@@ -2,7 +2,7 @@
 
 </script>
 
-<nav class="fixed flex justify-between px-8 h-16 items-center w-full bg-white/50 backdrop-blur-xl shadow border-b border-neutral-100/50">
+<nav class="fixed flex justify-between px-8 h-16 items-center w-full bg-white/50 backdrop-blur-xl shadow border-b border-neutral-100/20">
     <a href="/" class="text-2xl font-bold duration-300 hover:text-neutral-700 hover:brightness-125">Horse<span class="text-pink-500">Time</span></a>
     <div class="space-x-4">
         <a href="/" class="no-underline hover:underline hover:underline-offset-4">Home</a>

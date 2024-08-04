11958680d92907665c984fd0486cb09e976d7b71
lqjx <joel.muenster@proton.me>
2024-08-04 21:54:37 +0200

    Added Node Adapter

---
+++ src/lib/Home/AboutUsSlider.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+    import '@splidejs/svelte-splide/css';
+
+    import sliderImage1 from "$lib/assets/IMG-20240323-WA0014.jpg"
+</script>
+
+<div>
+    <Splide aria-label="..." options={{
+        type: "slide",
+        speed: 500,
+        rewindByDrag: true,
+        autoplay: true,
+        interval: 3000,
+        pauseOnHover: true,
+        rewind: true
+    }}>
+        <SplideSlide>
+            <img src={sliderImage1} alt="Image 1" class="m-auto rounded-lg drop-shadow-lg w-full h-96 object-cover object-center"/>
+        </SplideSlide>
+        <SplideSlide>
+            <img src="https://fakeimg.pl/400x200" alt="Image 2" class="m-auto rounded-lg drop-shadow-lg w-full h-96 object-cover object-center"/>
+        </SplideSlide>
+        <SplideSlide>
+            <img src="https://fakeimg.pl/500x300" alt="Image 2" class="m-auto rounded-lg drop-shadow-lg w-full h-96 object-cover object-center"/>
+        </SplideSlide>
+    </Splide>
+</div>
